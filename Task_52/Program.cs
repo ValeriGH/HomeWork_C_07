63b804ffbee1e55e6256c0ad66970e2bdcb7ae93
Valeria <valeria_korzun@mail.ru>
2022-12-19 19:48:51 +0300

    Среднее арифметическое столбцов

---
+++ Task_52/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задать двумерный массив целых чисел
+// Найти среднее арифметическое столбцов
+
+void InputArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 101);
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Solution(int[,] matrix)
+{
+    Console.WriteLine("\nСреднее арифметическое столбцов: ");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i,j];
+        }
+        Console.Write($"{sum/matrix.GetLength(0)}\t");
+    }
+}
+
+Console.Clear();
+int n = new Random().Next(1, 6);
+int m = new Random().Next(1, 6);
+Console.WriteLine($"Матрица размером {n} x {m}:");
+int[,] matrix = new int[n, m];
+InputArray(matrix);
+Solution(matrix);
